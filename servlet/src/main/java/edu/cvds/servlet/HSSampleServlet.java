a7d19125015b9c4faf65b012b3a3fbc2f5adb224
unknown <david.leon-m@labinfo.is.escuelaing.edu.co>
2022-03-05 09:42:42 -0500

    parte2_2

--- servlet/src/main/java/edu/cvds/servlet/HSSampleServlet.java
+++ servlet/src/main/java/edu/cvds/servlet/HSSampleServlet.java
@@ -13,45 +13,40 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import edu.cvds.servlet.model.Todo;
-
+@WebServlet(
+        urlPatterns = "/davidServlet"
+)
 public class HSSampleServlet extends HttpServlet{
 	static final long serialVersionUID = 35L;
-	public HSSampleServlet() {
-
-	}
-
     @Override
-   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    	
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Writer responseWriter = resp.getWriter();
         try {
-            Optional<String> optId = Optional.ofNullable(req.getParameter("entrada"));
-            Integer id = (Integer.parseInt(optId.get()));
-            Todo lista = Service.getTodo(id);           
-            ArrayList<Todo> listaDeToDos = new ArrayList<Todo>();
-            listaDeToDos.add(lista);
-            resp.setStatus(HttpServletResponse.SC_OK);
-            responseWriter.write(Service.todosToHTMLTable(listaDeToDos));
-            responseWriter.flush();              
+            Optional<String> optName = Optional.ofNullable(req.getParameter("id"));
+            if (optName.isPresent()) {
+                int id = Integer.parseInt(optName.get());
+                resp.setStatus(HttpServletResponse.SC_OK);
+                ArrayList<Todo> todos = new ArrayList<>();
+                Todo todo = Service.getTodo(id);
+                todos.add(todo);
+                responseWriter.write(Service.todosToHTMLTable(todos));
+            }
         }
-        //Id invalido (Se paso un String o no hubo valor)
-        catch ( NumberFormatException e){
-            responseWriter.write("Requerimiento Inválido1");
-        }
-        //Id No encontrado
         catch (FileNotFoundException e){
-            responseWriter.write("No encontrado.");
-            
-            
-            
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            responseWriter.write("No existe un item con el identificador dado");
         }
-        //MalformedURL
+        catch (NumberFormatException e){
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            responseWriter.write("El parametro no es un numero");
+        }
         catch (MalformedURLException e){
-            responseWriter.write("Error interno en el Servidor");
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            responseWriter.write("Error interno del servidor");
         }
-        //otros Errores
         catch (Exception e){
-            responseWriter.write("Requerimiento Invalido");
-        }  
-   }
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            responseWriter.write("Requerimiento invalido");
+        }
+    }
 }
