3aace7bf57c07bac5f9e88ecdc292235b6f9dd99
unknown <david.leon-m@labinfo.is.escuelaing.edu.co>
2022-03-05 09:23:06 -0500

    parte2

---
+++ servlet/src/main/java/edu/cvds/servlet/HSSampleServlet.java
@@ -0,0 +1,11 @@
+package edu.cvds.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+public class HSSampleServlet extends HttpServlet{
+
+	public HSSampleServlet() {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
